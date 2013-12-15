54aef18bba9d05dbb1317a736d50c23e4971dcf6
elliott5 <elliott@elliott-stonehams-macbook-pro.local>
2013-12-15 08:24:28 +0000

    Add Zilen() builtin function

--- hxbuiltin/api.go
+++ hxbuiltin/api.go
@@ -9,11 +9,16 @@ package hxbuiltin // This package contains functions over-loaded by the Go to Ha
 // ret is a Haxe Dynamic value mapped into Go as a uintptr
 func HAXE(code string) (ret uintptr) { return }
 
-// returns the Host language (e.g. "Go","Haxe"), the return value is overridden to give correct host language name
+// returns the Host language (i.e. "Go" or "Haxe"), the return value is overridden to give correct host language name
 func Host() string { return "Go" }
 
-// returns language specific the Platform information, the return value is overridden at runtime
+// Return language specific the Platform information, the return value is overridden at runtime
 // for "Haxe" as host this returns the target language platform as one of: "flash","js","neko","php","cpp","java","cs"
 func Platform() string { return "go" }
 
+// Return a string containing the Go code position in terms of file name and line number
 func CPos() string { return "<<go code pos>>" } // the return value is overwridden by the transpiler, here just for Go use
+
+// ZiLen() returns the runtime native string length of the chinese character "字", meaning "written character", which is pronounced "zi" in Mandarin.
+// For UTF8 encoding this value is 3, for UTF16 encoding this value is 1.
+func Zilen() int { return 3 }
