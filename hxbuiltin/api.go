1eff2638eebdd7e361df12d271c4ffbcafe8c5f3
elliott5 <elliott@elliott-stonehams-macbook-pro.local>
2013-12-16 22:02:35 +0000

    Add functions to hxbuiltin

--- hxbuiltin/api.go
+++ hxbuiltin/api.go
@@ -9,8 +9,8 @@ package hxbuiltin // This package contains functions over-loaded by the Go to Ha
 // ret is a Haxe Dynamic value mapped into Go as a uintptr
 func HAXE(code string) (ret uintptr) { return }
 
-// returns the Host language (i.e. "Go" or "Haxe"), the return value is overridden to give correct host language name
-func Host() string { return "Go" }
+// returns the Host language (i.e. "go" or "haxe"), the return value is overridden to give correct host language name
+func Host() string { return "go" }
 
 // Return language specific the Platform information, the return value is overridden at runtime
 // for "Haxe" as host this returns the target language platform as one of: "flash","js","neko","php","cpp","java","cs"
@@ -22,3 +22,19 @@ func CPos() string { return "<<go code pos>>" } // the return value is overwridd
 // Zilen() returns the runtime native string length of the chinese character "字", meaning "written character", which is pronounced "zi" in Mandarin.
 // For UTF8 encoding this value is 3, for UTF16 encoding this value is 1.
 func Zilen() uint { return 3 }
+
+// Is the native string encoding UTF8?
+func StringsUTF8() bool { return Zilen() == 3 }
+
+// Is the native string encoding UTF16?
+func StringsUTF16() bool { return Zilen() == 1 }
+
+/*
+	Replicant functions of the go "runtime" package
+*/
+
+// Schedule other goroutines.
+func Gosched() {} // an empty function here works fine to enable other goroutines to be scheduled
+
+// Number of goroutines (may be more than the number runable).
+func NumGoroutine() int { return int(HAXE("Scheduler.NumGoroutine();")) }
